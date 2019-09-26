6017b237dec5984c69a75a31b2879204d991a644
Connor Hock <cbh06002@creighton.edu>
2019-09-25 20:06:46 -0500

    Home Loan

--- forms/homeLoan/homeLoan.js
+++ forms/homeLoan/homeLoan.js
@@ -1,5 +1,9 @@
 // HOME LOAN
 
+let homeLoanAmount = Number(prompt("Please enter your loan amount (without $ sign)."));
+let homeLoanYears = Number(prompt("Please enter your mortage length (in years)."));
+let homeInterestRate = Number(prompt("Please enter your interest rate (without % sign)."));
+
 function homeLoanPayment(homeLoanAmount, homeLoanYears, homeInterestRate) {
     let homePaymentEquation = (homeLoanAmount / (homeLoanYears / 12)) * (1 + homeInterestRate);
     return homePaymentEquation;
@@ -8,13 +12,11 @@ function homeLoanPayment(homeLoanAmount, homeLoanYears, homeInterestRate) {
 let monthlyHomePayment = homeLoanPayment(homeLoanAmount, homeLoanYears, homeInterestRate);
 alert(`A home loan for ${homeLoanAmount} over ${homeLoanYears} years at a ${homeInterestRate} interest rate would have a monthly payment of ${monthlyHomePayment}.`);
 
-let homeLoanAmount = Number(prompt("Please enter your loan amount (without $ sign)."));
-let homeLoanYears = Number(prompt("Please enter your mortage length (in years)."));
-let homeInterestRate = Number(prompt("Please enter your interest rate (without % sign)."));
 
 
 
 /*
+
 if (homeLoanYears ===15 ) {
         alert("Thank you for selecting the monthly car loan payment calculator.");
     } else if (chooseCalculator == "house") {
