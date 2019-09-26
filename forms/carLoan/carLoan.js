3df0f8c1a4a23b07b050cfa05a5f5c5b4e477537
Connor Hock <cbh06002@creighton.edu>
2019-09-25 20:05:33 -0500

    Car Loan

--- forms/carLoan/carLoan.js
+++ forms/carLoan/carLoan.js
@@ -11,9 +11,3 @@ function carLoanPayment(carLoanAmount, carLoanMonths, carInterestRate) {
 
 let monthlyCarPayment = carLoanPayment(carLoanAmount, carLoanMonths, carInterestRate);
 alert(`A car loan for $${carLoanAmount} over ${carLoanMonths} months at a ${carInterestRate}% interest rate would have a monthly payment of $${monthlyCarPayment}.`);
-
-// ex: A car loan for $30,000 over 50 months at a 3.2% interest rate would have a monthly payment of ${monthlyCarPayment}.
-// (carLoanAmount / carLoanMonths) * ((carInterestRate / 100) + 1)
-// (30,000 / 50) * ((3.2 / 100) + 1)
-// (600) * (1.032)
-// = $619.20
