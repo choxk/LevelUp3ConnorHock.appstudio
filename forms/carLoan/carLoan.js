9a754952f031381296ad846f9cfa6a642fb96443
Connor Hock <cbh06002@creighton.edu>
2019-09-25 19:56:41 -0500

    Car Loan

--- forms/carLoan/carLoan.js
+++ forms/carLoan/carLoan.js
@@ -1,1 +1,15 @@
 // CAR LOAN
+
+let carLoanAmount = Number(prompt("Please enter your loan amount (without $ sign)."));
+let carLoanMonths = Number(prompt("Please enter your loan payment length (in number of months)."));
+let carInterestRate = Number(prompt("Please enter your interest rate (without % sign)."));
+
+function carLoanPayment(carLoanAmount, carLoanMonths, carInterestRate) {
+    let carPaymentEquation = (carLoanAmount / carLoanMonths) * (1 + carInterestRate);
+    return carPaymentEquation;
+};
+
+let monthlyCarPayment = carLoanPayment(carLoanAmount, carLoanMonths, carInterestRate);
+alert(`A car loan for $${carLoanAmount} over ${carLoanMonths} months at a ${carInterestRate}% interest rate would have a monthly payment of ${monthlyCarPayment}.`);
+
+// ex: A car loan for $30,000 over 50 months at a ${carInterestRate} interest rate would have a monthly payment of ${monthlyCarPayment}.
