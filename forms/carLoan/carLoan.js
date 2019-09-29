ee7686f7d4345ffaffa14e6fd8cc940c44a95ddb
Connor Hock <cbh06002@creighton.edu>
2019-09-29 18:47:10 -0500

    car loan

--- forms/carLoan/carLoan.js
+++ forms/carLoan/carLoan.js
@@ -1,5 +1,5 @@
 // CAR LOAN
-
+/*
 let carLoanAmount = Number(prompt("Please enter your loan amount (without $ sign)."));
 let carLoanMonths = Number(prompt("Please enter your loan payment length (in number of months)."));
 let carInterestRate = Number(prompt("Please enter your interest rate (without % sign)."));
@@ -13,4 +13,4 @@ let monthlyCarPayment = carLoanPayment(carLoanAmount, carLoanMonths, carInterest
 alert(`A car loan for $${carLoanAmount} over ${carLoanMonths} months at a ${carInterestRate}% interest rate would have a monthly payment of $${monthlyCarPayment.toFixed(2)}.`);
 
 
-// SHOULD BE: A car loan for $30,000 over 60 months at 3.2% interest would have a monthly payment of $516.00.
+// SHOULD BE: A car loan for $30,000 over 60 months at 3.2% interest would have a monthly payment of $516.00. */
