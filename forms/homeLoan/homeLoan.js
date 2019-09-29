bcc2589996dd66b8215c81b7baaefe2d7813c948
Connor Hock <cbh06002@creighton.edu>
2019-09-29 18:46:51 -0500

    home loan

--- forms/homeLoan/homeLoan.js
+++ forms/homeLoan/homeLoan.js
@@ -1,7 +1,7 @@
 // HOME LOAN
-
+/*
 let homeLoanAmount = Number(prompt("Please enter your loan amount (without $ sign)."));
-let homeLoanYears = Number(prompt("Please enter your mortage length (in years)."));  
+let homeLoanYears = Number(prompt("Please enter your mortage length (in years: either 15 or 30)."));  
 let homeInterestRate = Number(prompt("Please enter your interest rate (without % sign)."));
 
 function homeLoanPayment(homeLoanAmount, homeLoanYears, homeInterestRate) {
@@ -13,13 +13,4 @@ let monthlyHomePayment = homeLoanPayment(homeLoanAmount, homeLoanYears, homeInte
 alert(`A home loan for $${homeLoanAmount} over ${homeLoanYears} years at a ${homeInterestRate}% interest rate would have a monthly payment of $${monthlyHomePayment.toFixed(2)}.`);
 
 
-// SHOULD BE: A home loan for $200,000 over 30 years at 5.7% interest would have a monthly payment of $587.22.
-
-
-/*
-// Term can only be 15 or 30 years.
-let homeLoanYears = ;
-do{
-  var checkHomeYears = Number(prompt("Mortage length must 15 or 30 years. Please re-enter."));
-} while(checkHomeYears !== homeLoanYears)
-*/
+// SHOULD BE: A home loan for $200,000 over 30 years at 5.7% interest would have a monthly payment of $587.22. */
